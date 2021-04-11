daed404aaa93b015088db2fd1654f96294a97d33
Daanvb2001 <Daan.vanbijsterveld@student.hu.nl>
2021-04-11 22:34:24 +0200

    dependatabot

--- src/test/java/nl/hu/cisq1/lingo/trainer/application/GameServiceTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/application/GameServiceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
